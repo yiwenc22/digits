9664087434d98510fb1573b33cf1e019efb50da1
Your Name <yiwenc22@gmail.com>
2022-04-01 22:58:10 -1000

    contact forms finished in 18 minutes.

---
+++ app/imports/ui/pages/AddContact.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Contacts } from '../../api/contact/Contacts';
+
+/** Create a schema to specify the structure of the data to appear in the form. */
+const formSchema = new SimpleSchema({
+  firstName: String,
+  lastName: String,
+  address: String,
+  image: String,
+  description: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
+class AddContact extends React.Component {
+
+  /** On submit, insert the data. */
+  submit(data, formRef) {
+    const { firstName, lastName, address, image, description } = data;
+    const owner = Meteor.user().username;
+    Contacts.collection.insert({ firstName, lastName, address, image, description, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    let fRef = null;
+    return (
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center">Add Contact</Header>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+            <Segment>
+              <TextField name='firstName'/>
+              <TextField name='lastName'/>
+              <TextField name='address'/>
+              <TextField name='image'/>
+              <LongTextField name='description'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default AddContact;
